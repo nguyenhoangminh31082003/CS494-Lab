a3c1dc620397fba2ecaa4486594d351729562084
Nhu-Binh Nguyen Truc <ntnbinh21@apcs.fitus.edu.vn>
2024-04-12 09:07:40 +0700

    change background and add restart button

--- GameGUI/AssetConstants.py
+++ GameGUI/AssetConstants.py
@@ -8,5 +8,9 @@ BACKGROUND  = image.load(os.path.join(ASSET_PATH, 'background.png'))
 MENU        = image.load(os.path.join(ASSET_PATH, 'menu.png'))
 PLAY_BUTTON = image.load(os.path.join(ASSET_PATH, 'playButton.png'))
 
+INTRO = image.load(os.path.join(ASSET_PATH, 'start.png'))
+INGAME = image.load(os.path.join(ASSET_PATH, 'ingame.png'))
+CHEST = image.load(os.path.join(ASSET_PATH, 'chest.png'))
+
 AMATICSC_FONT       = os.path.join(ASSET_PATH, 'Nunito.ttf')
 VCR_OSD_MONO_FONT   = os.path.join(ASSET_PATH, 'Nunito.ttf')
